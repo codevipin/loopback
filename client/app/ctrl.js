3bda614652517c20c1f31b041e5d39158de1780e
Vipin Verma <vipinverma@live.com>
2016-06-23 07:25:32 +0530

    update ctrl

--- client/app/ctrl.js
+++ client/app/ctrl.js
@@ -1,55 +1,78 @@
 app
-	.controller('formCtrl',['$scope','regService','loginServe','$state',function($scope,regService,loginServe,$state){
+	.controller('formCtrl',['$scope','loginServe','$state','$rootScope',function($scope,loginServe,$state,$rootScope){
 		$scope.showError = false;
-		$scope.formFunc = function(){
-			console.log($scope.form);
-		regService.update({id:$scope.form.id},$scope.form);
-		$scope.form = "";
-		}
+  		$rootScope.showErr = false;
+		$scope.loginFn = function() {
 
-		// $scope.showReg = regService.query({},
-            
-  //                           function(response){
-  //                               $scope.showReg = response;
-  //                               console.log($scope.showReg);
-                                
-                                
-  //                           },
-  //                           function(response) {
-  //                               $scope.message = "Error: "+response.status + " " + response.statusText;
-  //                           });
-  		
-		$scope.loginFn = function(){
-			
-			$scope.detail=loginServe.update({id:$scope.login.id},$scope.login).$promise.then(
-					function(response){
-						$scope.detail = response;
-						console.log($scope.detail);
-			
-						console.log($scope.login.email);
-						if(email=='admin@admin.com'){
-						$state.go('register');
-							}
-						
-					},
-					function(response){
-						console.log(response.status,response.statusText);
-						if(response.status==401){
-							$scope.showError = true;
-							$scope.error = "Wrong Username or Password";
-								
-								    setTimeout(function(){
-								     $scope.showError = false;
-								     $scope.$apply();
-								      }, 3000);
-								
-						}
+			$scope.detail=loginServe.login().update({id:$scope.login.id},$scope.login).$promise.then(
+				function(response) {
+
+					$scope.detail = response;
+					console.log($scope.detail.id);
+					$scope.token = $scope.detail.id;
+					$rootScope.showErr = true;
+					
+
+					// localStorage.setItem("token", JSON.stringify($scope.detail.id));
+					$state.go('register');
+				},
+				function(response){
+
+					console.log(response.status,response.statusText);
+					if(response.status==401){
+
+						$scope.showError = true;
+						$scope.error = "Wrong Username or Password";
+
+					    setTimeout(function(){
+
+					     $scope.showError = false;
+					     $scope.$apply();
+					     }, 3000);
+							
 					}
-					);	
-		}
-		
-		
+				}
+			);	
+		};
 
-	}]);
+		$scope.logoutFn = function(){
+			var token = $rootScope.showErr;
+			//loginServe.update({})
+			//$scope.token=$scope.detail.id;
+			// var getToken = localStorage.getItem("swagger_accessToken");
+			// console.log(getToken);
+			$state.go('app');
+			console.log("Logged out,token Deleted"); 
+
+			// $scope.logoutDetail = loginServe.logout($scope.token).update().$promise.then(function(response) {
+			// 	$scope.logoutDetail = response;
+			// 	console.log($scope.logoutDetail);
+			// 	},
+			// 	function(response){
+			// 	console.log(response.status,response.statusText);
+			// 	}
+			// 	)
+		};
+
+		$scope.regSubmit =  function()	{
+			console.log($scope.regform);
+			$scope.driverDetail = loginServe.regDriver().update({id:$scope.regform.id},$scope.regform).$promise.then(function(response) {
+				$scope.driverDetail = response;
+				console.log($scope.driverDetail);
+				},
+				function(response){
+				console.log(response.status,response.statusText);
+				}
+				)
+				
+		};
+
+		}]);
+
+
+
+
+
+
 
 
