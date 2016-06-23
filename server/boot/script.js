3c9844a2ff09897adbbee1275bc7a9ea44a7ddd0
Ubuntu <ubuntu@ip-172-31-50-236.ec2.internal>
2016-06-23 02:31:09 +0000

    service api

--- server/boot/script.js
+++ server/boot/script.js
@@ -15,7 +15,7 @@ MongoDB.automigrate('regtest', function(err) {
 
     //create the admin role
     Role.create({
-      name: 'admin6'
+      name: 'admin33'
     }, function(err, role) {
       if (err) throw (err);
        //make admin
